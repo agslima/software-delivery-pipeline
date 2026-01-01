5de54f90bb9a47b34190a3a3ac1d9e73912a339e
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2026-01-01 15:25:11 +0000

    Fix issues workflows

--- app/server/index.js
+++ app/server/index.js
@@ -4,6 +4,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on ${PORT}`);
 });
