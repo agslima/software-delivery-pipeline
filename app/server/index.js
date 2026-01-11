37f6964154a8df5acd71ccb81c26de47d3953206
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2026-01-10 23:23:09 -0300

    Update index.js

--- app/server/index.js
+++ app/server/index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on ${PORT}`);
