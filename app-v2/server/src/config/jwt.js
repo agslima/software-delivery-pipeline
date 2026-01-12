94a6c0ca5daff7948286b81e7c9c536d317b831b
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2026-01-12 18:43:28 -0300

    Update jwt.js

--- app-v2/server/src/config/jwt.js
+++ app-v2/server/src/config/jwt.js
@@ -1,7 +1,6 @@
 const env = require('./env'); // Import validated config
 
 module.exports = {
-  // We can safely access env.JWT_SECRET because envalid guarantees it exists
   secret: env.JWT_SECRET,
   expiresIn: '1h',
 };
