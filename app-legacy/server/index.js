cba067916eda4cd17b284a255ebe05051f03a261
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2026-03-07 16:32:02 -0300

    remove old files

--- app-legacy/server/index.js
+++
@@ -1,9 +0,0 @@
-// server/index.js
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on ${PORT}`);
-});
